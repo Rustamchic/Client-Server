87fa9206ea854ed8e7a6e6f90ca3ffd241011ede
Rustamchic <73492620+Rustamchic@users.noreply.github.com>
2023-06-22 15:10:27 +0300

    Update ClientC.cpp

--- ClientC/ClientC.cpp
+++ ClientC/ClientC.cpp
@@ -9,90 +9,110 @@ using namespace std;
 SOCKET Connection;
 enum Packet
 {
-	P_ChatMessage,
-	P_Test
+    P_ChatMessage,
+    P_Test
 };
 bool ProcessPacket(Packet packettype)
 {
-	switch (packettype)
-	{
-	case P_ChatMessage:
-	{
-		int msg_size;
-		recv(Connection, (char*)&msg_size, sizeof(int), NULL);
-		char* msg = new char[msg_size + 1];
-		msg[msg_size] = '\0';
-		recv(Connection, msg, msg_size, NULL);
-		cout << msg << endl;
-		delete[] msg;
-		break;
-	}
-	case P_Test:
-	{
-		cout << "Test packet" << endl;
-		break;
-	}
-	default:
-		cout << "Unrecognized! packet" << packettype << endl;
-	}
-	return true;
+    switch (packettype)
+    {
+    case P_ChatMessage:
+    {
+        int msg_size;
+        recv(Connection, (char*)&msg_size, sizeof(int), NULL);
+        char* msg = new char[msg_size + 1];
+        msg[msg_size] = '\0';
+        recv(Connection, msg, msg_size, NULL);
+        cout << msg << endl;
+        delete[] msg;
+        break;
+    }
+    case P_Test:
+    {
+        cout << "Test packet" << endl;
+        break;
+    }
+    default:
+        cout << "Unrecognized packet: " << packettype << endl;
+    }
+    return true;
 }
+
 void ClientHandler()
 {
-	Packet packettype;
-	while (true)
-	{
-		recv(Connection, (char*)&packettype, sizeof(Packet), NULL);
-		if (!ProcessPacket(packettype))
-		{
-			break;
-		}
-	}
-	closesocket(Connection);
+    Packet packettype;
+    while (true)
+    {
+        if (recv(Connection, (char*)&packettype, sizeof(Packet), NULL) != SOCKET_ERROR)
+        {
+            if (!ProcessPacket(packettype))
+                break;
+        }
+        else
+        {
+            int error = WSAGetLastError();
+            if (error == WSAECONNRESET)
+            {
+                cout << "Server closed the connection." << endl;
+                break;
+            }
+            else
+            {
+                cout << "Error receiving packet. Error code: " << error << endl;
+                break;
+            }
+        }
+    }
+    closesocket(Connection);
 }
 
 int main()
 {
-	// Hide console window
-	HWND stealth;
-	AllocConsole();
-	stealth = FindWindowA("ConsoleWindowClass", NULL);
-	ShowWindow(stealth, 0);
+    WSAData wsaData;
+    WORD DLLVersion = MAKEWORD(2, 2);
+    if (WSAStartup(DLLVersion, &wsaData) != 0)
+    {
+        cout << "Failed to initialize winsock." << endl;
+        return 1;
+    }
+    SOCKET Connection = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (Connection == INVALID_SOCKET)
+    {
+        cout << "Failed to create socket. Error code: " << WSAGetLastError() << endl;
+        WSACleanup();
+        return 1;
+    }
+    SOCKADDR_IN addr;
+    int sizeofaddr = sizeof(addr);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    addr.sin_port = htons(1234); 
+    addr.sin_family = AF_INET;
+    if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR)
+    {
+        cout << "Failed to connect to server. Error code: " << WSAGetLastError() << endl;
+        closesocket(Connection);
+        WSACleanup();
+        return 1;
+    }
 
-	// WSASrartup
-	WSAData wsaData;
-	WORD DLLVersion = MAKEWORD(2, 1);
-	if (WSAStartup(DLLVersion, &wsaData) != 0)
-	{
-		cout << "Error!" << endl;
-		exit(1);
-	}
-	SOCKADDR_IN addr;
-	int sizeofaddr = sizeof(addr);
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	addr.sin_port = htons(1111);
-	addr.sin_family = AF_INET;
-	Connection = socket(AF_INET, SOCK_STREAM, NULL);
-	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0)
-	{
-		cout << "Failed to connect!" << endl;
-		return 1;
-	}
-	else
-	{
-		cout << "Connected!" << endl;
-		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, NULL, NULL, NULL);
-		string msg1;
-		while (true)
-		{
-			getline(cin, msg1);
-			int msg_size = msg1.size();
-			Packet packettype = P_ChatMessage;
-			send(Connection, (char*)&packettype, sizeof(Packet), NULL);
-			send(Connection, (char*)&msg_size, sizeof(int), NULL);
-			send(Connection, msg1.c_str(), msg_size, NULL);
-			Sleep(10);
-		}
-	}
-	return 0;
+    cout << "Connected to the server!" << endl;
+
+    CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, NULL, NULL, NULL);
+
+    string msg;
+    while (true)
+    {
+        getline(cin, msg);
+        int msg_size = msg.size();
+        Packet packettype = P_ChatMessage;
+        send(Connection, (char*)&packettype, sizeof(Packet), NULL);
+        send(Connection, (char*)&msg_size, sizeof(int), NULL);
+        send(Connection, msg.c_str(), msg_size, NULL);
+        Sleep(10);
+    }
+
+    closesocket(Connection);
+    WSACleanup();
+
+    return 0;
 }
